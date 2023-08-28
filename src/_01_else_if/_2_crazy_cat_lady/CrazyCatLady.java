7b214a44020d1e4f9749ad9925f557edb6ba0be9
Programmer628 <128913408+Programmer628@users.noreply.github.com>
2023-08-28 14:46:05 -0700

    Update CrazyCatLady.java

--- src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
+++ src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
@@ -6,20 +6,15 @@ import java.net.URI;
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		
-		// 1. Ask the user how many cats they have
 		String catNumber = JOptionPane.showInputDialog("How many cats do you have (In numbers please).");
-		// 2. Convert their answer into an int
 		int catAsInt = Integer.parseInt(catNumber);
-		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
 		if(catAsInt >= 3){
 		JOptionPane.showMessageDialog(null, "You are a crazy cat lady");
-		} else if(catAsInt < 3){
+		} else if(catAsInt < 3 > 0){
 		playVideo(https://www.youtube.com/watch?v=4JB20_BJJnE);
 		} else if(catAsInt = 0){
-			JOptionPane.
-		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
-		//    Hint: You can use the && operator 
-		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
+			JOptionPane.showMessageDialog(null, "You need to get a cat, but no more than 3.");
+		}
 		
 		
 		
